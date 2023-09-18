f5729500f8d4226a1e9190fd8216b77afc3a6944
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-18 16:52:48 -0500

    Initial commit with empty project

---
+++ src/main/java/com/jp/gateway/JpSpringGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpSpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpSpringGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/gateway/JpSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpSpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
