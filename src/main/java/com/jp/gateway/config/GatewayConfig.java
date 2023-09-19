70c3569e1c26dff2d944b7015a791b85d1e2b9b8
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-19 16:06:58 -0500

    Add circuit breker in case of failure of one of the instance of the config ms

--- src/main/java/com/jp/gateway/config/GatewayConfig.java
+++ src/main/java/com/jp/gateway/config/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
@@ -33,6 +35,36 @@ public class GatewayConfig {
                 .route(r -> r.path("/api/v1/got/*").uri("lb://JP-ANOTHER-MS"))
                 .route(r -> r.path("/application-name").uri("lb://JP-CONFIG"))
                 .build();
+    }
+
+    @Bean
+    @Profile("localhostRouter-EurekaWithCB")
+    public RouteLocator configLocalWithEurekaAndCircuitBreaker(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(r -> r.path("/api/v1/dragonball/*").uri("lb://JP-OTHER-MS"))
+                .route(r -> r.path("/api/v1/got/*").uri("lb://JP-ANOTHER-MS"))
+
+                // Aquí enrutamos a // application-name que està definido en la uri de eureka: lb://JP-CONFIG,
+                .route(r -> r.path("/application-name")
+                        // colocamos un filtro, y en caso de falla, vamos a hacer un 'forward' hacia
+                        // el path '/api/v1/dragonball/names' que está definido en eureka: lb://JP-FAILOVER-MS
+                        .filters(f -> {
+                            f.circuitBreaker(
+                                            c -> c.setName("failoverCB")
+                                                    .setFallbackUri("forward:/api/v1/dragonball/names") // en caso de falla, se va por aquí
+                                                    .setRouteId("dbFailover"))
+                                    .uri("lb://JP-FAILOVER-MS");
+                            return f;
+                        }).uri("lb://JP-CONFIG"))
+                .build();
+
+    }
+
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 
 }
